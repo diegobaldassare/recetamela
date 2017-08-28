7c2cdab3b82f0ed8100365470205ff9fd32eced2
niklabaz <nxbaez@gmail.com>
2017-08-28 18:58:22 -0300

    Recipe Controller --skip-ci

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import play.mvc.Controller;
+import play.mvc.Result;
 
-public class RecipeController extends Controller{
+public class RecipeController extends Controller {
 
+    public Result createRecipe() {
+        final JsonNode req = request().body().asJson();
 
+        return ok();
+    }
 }

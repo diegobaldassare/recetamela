0286839fbe0e86993299c2f0488935dbf5bd5dad
Matias Cicilia <matias.cicilia@ing.austral.edu.ar>
2017-08-30 12:45:11 -0300

    UserService and UserController --skip-ci

---
+++ app/services/UserService.java
@@ -0,0 +1,28 @@
+package services;
+
+import com.avaje.ebean.Model;
+import models.User;
+
+import java.util.Optional;
+
+/**
+ * Created by Matias Cicilia on 30-Aug-17.
+ */
+public class UserService extends Service<User> {
+
+    private static UserService instance;
+
+    protected UserService(Model.Finder<Long, User> finder) {
+        super(finder);
+    }
+
+    public static UserService getInstance() {
+        if (instance == null)
+            instance = new UserService(new Model.Finder<>(User.class));
+        return instance;
+    }
+
+    public Optional<User> getById(Long id) {
+        return Optional.ofNullable(User.find.select("id").where().eq("id", id).findUnique());
+    }
+}

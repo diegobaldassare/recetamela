083a06d37c905909047d3df6ec76f158a8b52d95
niklabaz <nxbaez@gmail.com>
2017-08-30 23:23:34 -0300

    Return exception message as JSON --skip-ci

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -5,6 +5,7 @@ import models.recipe.Recipe;
 import models.recipe.json.RecipeInputJson;
 import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import server.exception.BadRequestException;
 
@@ -17,7 +18,7 @@ public class RecipeController extends Controller {
             final Recipe recipe = RecipeService.getInstance().save(recipeInput);
             return ok(Json.toJson(recipe));
         } catch (BadRequestException e) {
-            return badRequest(Json.toJson(e.getRequestError()));
+            return badRequest(e.getMessage()).as(Http.MimeTypes.JSON);
         }*/
         return ok();
     }

6a37ddbdc4f338f6677763df21263a83f8ef347e
niklabaz <nxbaez@gmail.com>
2017-09-03 17:05:36 -0300

    Fixed media service in ui --skip-ci

--- ui/src/app/shared/services/media.service.ts
+++ ui/src/app/shared/services/media.service.ts
@@ -11,7 +11,6 @@ export class MediaService {
   public uploadMedia(file: Blob): Promise<any> {
     const data = new FormData();
     data.append('file', file);
-    return this.http.post("http://localhost:9000/api/media", data)
-      .map((r: Response) => r.json()).toPromise();
+    return this.http.post("http://localhost:9000/api/media", data).toPromise();
   }
 }

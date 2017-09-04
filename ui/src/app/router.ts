bc1dcbf10aea5bc5a2df3bfb17f1cde8fea3af74
Bruno Ledesma <bruno.ledesma96@gmail.com>
2017-09-04 16:42:03 -0300

    Fix Merge --skip-ci

--- ui/src/app/router.ts
+++ ui/src/app/router.ts
@@ -4,7 +4,6 @@ import { LandingComponent } from "./landing/landing.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import {RecipesComponent} from "./recipes/recipes.component";
 import {ViewRecipeComponent} from "./recipes/view-recipe/view-recipe.component";
-import {NewRecipeComponent} from "./recipes/new-recipe/new-recipe.component";
 import {HomeComponent} from "./home/home.component";
 import {CreateRecipeComponent} from "./recipes/create-recipe/create-recipe.component";
 
@@ -12,7 +11,7 @@ const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'home', component: HomeComponent},
   { path: 'recipes', component: RecipesComponent },
-  { path: 'recipes/new', component: NewRecipeComponent },
+  { path: 'recipes/new', component: CreateRecipeComponent },
   { path: 'recipes/:id', component: ViewRecipeComponent },
   { path: '**', component: NotFoundComponent },
 ];

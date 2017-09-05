0a1f4bb68dc0979b27a63f3ad34c79701a240de8
Nicolas <nicolas.velazquez@ing.austral.edu.ar>
2017-09-05 17:05:34 -0300

    UpgradeToPremium Form working correctly --skip-ci

--- ui/src/app/nav/nav.component.ts
+++ ui/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-nav',
@@ -7,12 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  creditCardForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.creditCardForm = new FormGroup({
+      'cardName': new FormControl(null, [Validators.required]),
+      'cardNumber': new FormControl(null, [Validators.required]),
+      'cardCode': new FormControl(null, [Validators.required]),
+    });
   }
 
   upgradeToPremium(){
-    console.log("Funca");
+    this.createCreditCardForm();
+    /*this.cardService.create(this.createCreditCardForm());*/
+  }
+
+  private createCreditCardForm(){
+    const cardName = this.creditCardForm.value.cardName;
+    const cardNumber = this.creditCardForm.value.cardNumber;
+    const cardCode = this.creditCardForm.value.cardCode;
+
+    console.log(cardName);
+    console.log(cardNumber);
+    console.log(cardCode);
+    //return new Card(cardName, cardNumber, cardCode);
   }
 }

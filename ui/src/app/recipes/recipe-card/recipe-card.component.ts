3c6d58b86e149a43db4edb28caa5ed6d7b5d2e8e
Diego <diegobalda1234@gmail.com>
2017-09-14 06:12:59 -0300

    Added recipe-card, recipe-list and not-logged (modal). TODO: get recipe-card content from database. --skip-ci

---
+++ ui/src/app/recipes/recipe-card/recipe-card.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.css']
+})
+export class RecipeCardComponent implements OnInit {
+
+  @Input() recipe;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

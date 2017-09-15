23e25d9669d65e7a8f312ea80561ffc69327e346
Matias Cicilia <matias.cicilia@ing.austral.edu.ar>
2017-09-14 23:53:57 -0300

    Nav component shows name and profile picture --skip-ci

--- ui/src/app/nav/nav.component.ts
+++ ui/src/app/nav/nav.component.ts
@@ -4,6 +4,7 @@ import {SharedService} from "../shared/services/shared.service";
 import {isNull} from "util";
 import {AsyncPipe} from "@angular/common";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../shared/models/user-model";
 
 @Component({
   selector: 'app-nav',
@@ -14,6 +15,7 @@ export class NavComponent implements OnInit {
 
   isLoggedIn: boolean;
   isPremium: boolean;
+  user : User;
 
   //Both SharedService and ChangeDetectorRef are necessary to listen to changes on logged in variable to show different nav.
   constructor(private auth: MyAuthService, private sharedService: SharedService, private cdRef: ChangeDetectorRef) {
@@ -22,6 +24,16 @@ export class NavComponent implements OnInit {
       if (res.hasOwnProperty('loggedIn')) {
         this.isLoggedIn = res.loggedIn;
         this.cdRef.detectChanges();
+        if (this.isLoggedIn) {
+          auth.requestLoggedUser().then((user : User) => {
+            localStorage.setItem("user", JSON.stringify(user));
+            this.user = user;
+            if (user.type === 'PremiumUser') this.isPremium = true;
+            this.cdRef.detectChanges();
+          });
+        } else {
+        }
+        //auth.loggedUser.then(res => {this.user = res});
       }
     });
   }
@@ -30,7 +42,14 @@ export class NavComponent implements OnInit {
     this.auth.logout();
   }
 
+  public crearReceta() {
+    if (this.isPremium) {
+
+    }
+  }
+
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem("user")) as User;
     // this.isPremium = this.auth.isPremium():
   }
 

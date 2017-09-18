ad62a7ba7c859e5543d5a1578587f21916b91fdb
Batto <tfbattolla@sirius.com>
2017-09-17 22:14:05 -0300

    add environment to api service

--- ui/src/app/shared/services/api-service.ts
+++ ui/src/app/shared/services/api-service.ts
@@ -1,3 +1,4 @@
+import {environment} from "../../../environments/environment";
 export abstract class ApiService {
-  public static API_URL: string = "http://localhost:9000/api";
+  public static API_URL: string = environment.production ? "http://recetamela.herokuapp.com/api" : "http://localhost:9000/api";
 }

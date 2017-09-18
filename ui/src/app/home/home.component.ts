540c3537891e4ef6a278adebbdb6ea67d58bd6c6
niklabaz <nxbaez@gmail.com>
2017-09-18 04:22:24 -0300

    Temporal bug fix --skip-ci

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,29 +1,34 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {SharedService} from "../shared/services/shared.service";
 import {MyAuthService} from "../auth/my-auth-service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
 
   public loggedIn: boolean = this.authService.isLoggedIn();
+  public sub: Subscription;
 
   constructor(
     private authService: MyAuthService,
     private sharedService: SharedService,
     private cdRef: ChangeDetectorRef
   ) {
-    this.sharedService.notifyObservable$.subscribe((res) => {
+    this.sub = this.sharedService.notifyObservable$.subscribe((res) => {
       if (res.hasOwnProperty('loggedIn')) {
+        window.location.reload(); //TODO fix
         this.loggedIn = res.loggedIn;
         this.cdRef.detectChanges();
       }
     });
   }
 
-  ngOnInit() {}
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
 }

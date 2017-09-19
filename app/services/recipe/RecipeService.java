0fed9d49611b94dbab814c09800262aa7473df34
niklabaz <nxbaez@gmail.com>
2017-09-18 21:14:09 -0300

    Arregle lo de los pasos al modificar la receta y ahora acepta url de youtube correctas. --skip-ci

--- app/services/recipe/RecipeService.java
+++ app/services/recipe/RecipeService.java
@@ -1,9 +1,14 @@
 package services.recipe;
 
 import com.avaje.ebean.Model.Finder;
+import models.Media;
 import models.recipe.Recipe;
 import models.recipe.RecipeStep;
+import services.MediaService;
 import services.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipeService extends Service<Recipe> {
 
@@ -25,12 +30,23 @@ public class RecipeService extends Service<Recipe> {
         if (input.getIngredients() != null) r.setIngredients(input.getIngredients());
         if (input.getImages() != null) r.setImages(input.getImages());
         if (input.getDifficulty() != 0) r.setDifficulty(input.getDifficulty());
-        if (input.getSteps() != null) {
-            for (RecipeStep s : r.getSteps()) s.delete();
-            r.setSteps(input.getSteps());
-        }
+        if (input.getSteps() != null) setSteps(r, input.getSteps());
         if (input.getVideoUrl() != null) r.setVideoUrl(input.getVideoUrl());
         if (input.getServings() != 0) r.setServings(input.getServings());
         if (input.getDuration() != 0) r.setDuration(input.getDuration());
+    }
+
+    private void setSteps(Recipe r, List<RecipeStep> steps) {
+        final List<Media> imagesToDelete = new ArrayList<>();
+        for (final RecipeStep s : r.getSteps()) {
+            if (s.getImage() != null) imagesToDelete.add(s.getImage());
+            s.setImage(null);
+            s.delete();
+        }
+        for (final RecipeStep s : steps)
+            if (s.getImage() != null && imagesToDelete.contains(s.getImage()))
+                imagesToDelete.remove(s.getImage());
+        for (final Media i : imagesToDelete) MediaService.getInstance().delete(i.getId());
+        r.setSteps(steps);
     }
 }

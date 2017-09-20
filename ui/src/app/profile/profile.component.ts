cebe5f9ec5542ac8f8c7c4faa7bed3063f640971
Diego <diegobalda1234@gmail.com>
2017-09-20 11:06:58 -0300

    Removed unused imported Router from profile.component.ts

--- ui/src/app/profile/profile.component.ts
+++ ui/src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 import {User} from "../shared/models/user-model";
 import {UserService} from "../shared/services/user.service";
 

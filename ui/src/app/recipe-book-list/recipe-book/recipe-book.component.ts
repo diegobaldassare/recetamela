b318a6020dd59cd4a8f436a6996f38bde866cc2e
Nicolas <nicolas.velazquez@ing.austral.edu.ar>
2017-09-29 19:31:00 -0300

    UI recepieBook --skip-ci

--- ui/src/app/recipe-book-list/recipe-book/recipe-book.component.ts
+++ ui/src/app/recipe-book-list/recipe-book/recipe-book.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {RecipeBookService} from "../../shared/services/recipebook.service";
 import {RecipeBook} from "../../shared/models/recipe/recipebook";
@@ -14,6 +14,7 @@ export class RecipeBookComponent implements OnInit {
 
   recipeBookId: string;
   recipeBook: RecipeBook = new RecipeBook();
+  @ViewChild('closeBtn') closeBtn: ElementRef;
 
   private recipeBookForm: FormGroup;
 
@@ -44,18 +45,19 @@ export class RecipeBookComponent implements OnInit {
   modifyRecipeBook(){
     console.log(this.recipeBookForm.value.recipeBookName);
     this.recipeBook.name = this.recipeBookForm.value.recipeBookName;
-    // let newRecipeBook: RecipeBook = new RecipeBook();       //Fijarme si puedo igualar el nuevo al viejo y solo cambiar el nombre
-    // newRecipeBook.name = this.recipeBookForm.value.recipeBookName;
-    // newRecipeBook.recipes = this.recipeBook.recipes;
-    // newRecipeBook.id = this.recipeBookId;
-    // newRecipeBook.creator = this.recipeBook.creator;
-    // this.recipeBookService.update(this.recipeBookId, newRecipeBook).then(() => {    //No esta funcionando el update
-    //   this.toaster.pop('success', 'Recetario Modificado');
-    // }, () => {
-    //   this.toaster.pop('error', 'No se ha podido modificar el recetario');
-    // });
+    let newRecipeBook: RecipeBook = new RecipeBook();       //Fijarme si puedo igualar el nuevo al viejo y solo cambiar el nombre
+    newRecipeBook.name = this.recipeBookForm.value.recipeBookName;
+    newRecipeBook.recipes = this.recipeBook.recipes;
+    newRecipeBook.id = this.recipeBookId;
+    newRecipeBook.creator = this.recipeBook.creator;
+    this.recipeBookService.update(this.recipeBookId, newRecipeBook).then(() => {    //No esta funcionando el update
+      this.toaster.pop('success', 'Recetario Modificado');
+    }, () => {
+      this.toaster.pop('error', 'No se ha podido modificar el recetario');
+    });
 
-    // this.recipeBookForm.reset();
+    this.recipeBookForm.reset();
+    this.closeBtn.nativeElement.click();
   }
 
 }

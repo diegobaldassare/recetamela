df4699945b041e193a87576686ab131efcb60f02
Diego <diegobalda1234@gmail.com>
2017-11-01 05:55:13 -0300

    Adjusted truncate pipe for news description

--- ui/src/app/shared/pipes/truncate.pipe.ts
+++ ui/src/app/shared/pipes/truncate.pipe.ts
@@ -5,10 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
   transform(value: string, limit = 25, completeWords = false, ellipsis = '...') {
-    if (completeWords) {
-      limit = value.substr(0, limit).lastIndexOf(' ');
+    if (value.length > limit) {
+      if (completeWords) {
+        limit = value.substr(0, limit).lastIndexOf(' ');
+      }
+      return `${value.substr(0, limit)}${ellipsis}`;
     }
-    return `${value.substr(0, limit)}${ellipsis}`;
+    else return value;
   }
 }
 
